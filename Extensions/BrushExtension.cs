ca0ebeabdc73366e184224e48af6499657149bc9
Maurice-86 <1556132522@qq.com>
2025-01-27 02:36:36 +0800

    添加扩展

---
+++ Extensions/BrushExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MusicPlayer.Extensions
+{
+    public static class BrushExtension
+    {
+        /// <summary>
+        /// SolidColorBrush 画刷颜色取反
+        /// </summary>
+        /// <param name="originalBrush"></param>
+        /// <returns></returns>
+        public static SolidColorBrush Invert(this SolidColorBrush originalBrush)
+        {
+            Color originalColor = originalBrush.Color;
+            Color invertedColor = Color.FromArgb(
+                originalColor.A,
+                (byte)(255 - originalColor.R),
+                (byte)(255 - originalColor.G),
+                (byte)(255 - originalColor.B)
+            );
+            return new SolidColorBrush(invertedColor);
+        }
+    }
+}
